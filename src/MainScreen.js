b821f4daaa9e639a4f85847161e5b26cfbd1c3ab
Koray Erdem KILIÇ <developerkoray@gmail.com>
2023-04-25 15:49:20 +0300

    available products card is OK

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -4,16 +4,16 @@ import Checkbox from 'expo-checkbox';
 
 const MainScreen = () => {
   const [checkboxes, setCheckboxes] = useState([
-    { id: 1, label: 'Checkbox 1', checked: false },
-    { id: 2, label: 'Checkbox 2', checked: false },
-    { id: 3, label: 'Checkbox 3', checked: false },
-    { id: 4, label: 'Checkbox 4', checked: false },
-    { id: 5, label: 'Checkbox 5', checked: false },
-    { id: 6, label: 'Checkbox 6', checked: false },
-    { id: 7, label: 'Checkbox 7', checked: false },
-    { id: 8, label: 'Checkbox 8', checked: false },
-    { id: 9, label: 'Checkbox 9', checked: false },
-    { id: 10, label: 'Checkbox 10', checked: false },
+    { id: 1, label: 'Checkbox 1', checked: false, groupIndex:0 },
+    { id: 2, label: 'Checkbox 2', checked: false, groupIndex:0 },
+    { id: 3, label: 'Checkbox 3', checked: false, groupIndex:0 },
+    { id: 4, label: 'Checkbox 4', checked: false, groupIndex:0 },
+    { id: 5, label: 'Checkbox 5', checked: false, groupIndex:0 },
+    { id: 6, label: 'Checkbox 6', checked: false, groupIndex:0 },
+    { id: 7, label: 'Checkbox 7', checked: false, groupIndex:0 },
+    { id: 8, label: 'Checkbox 8', checked: false, groupIndex:0 },
+    { id: 9, label: 'Checkbox 9', checked: false, groupIndex:0 },
+    { id: 10, label: 'Checkbox 10', checked: false, groupIndex:0 },
   ]);
 
   const handleCheckboxChange = (id) => {
@@ -28,7 +28,10 @@ const MainScreen = () => {
 
   return (
     <View style={styles.container}>
-      {checkboxes.map((checkbox) => (
+ <View style={styles.availableProductsCard}>
+<Text style={styles.availableProductsText}>Available Products</Text>
+ <View style={styles.group0}>
+ {checkboxes.map((checkbox) => (
         <View style={styles.checkBoxContainer} key={checkbox.id}>
           <Checkbox
             value={checkbox.checked}
@@ -37,6 +40,10 @@ const MainScreen = () => {
           <Text style={styles.checkboxText}>{checkbox.label}</Text>
         </View>
       ))}
+ </View>
+
+      </View>
+
     </View>
   );
 };
@@ -54,5 +61,22 @@ const styles = StyleSheet.create({
     checkboxText:{
         marginLeft:5,
         marginVertical:5
-    }
+    },
+    availableProductsCard:{
+      borderWidth:1,
+      padding:10,
+      borderColor:'#d5d5d5',
+      backgroundColor:'white' 
+    },
+    availableProductsText:{
+      textAlign:'center',
+      fontWeight:'500',
+      padding:10,
+      color:'green'
+    },
+    group0:{
+      flexWrap:'wrap',
+      flexDirection:'row',
+      justifyContent:'space-between',
+    },
 })
